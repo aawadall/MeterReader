fdd8b2ab0082be569b44016394006fc0e191da4e
Ahmed Awadallah <aawadall@ualberta.ca>
2020-02-02 12:54:42 -0500

    add basic service

---
+++ src/MeterReader/MeterReaderWeb/Services/MeterService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace MeterReaderWeb.Services
+{
+    public class MeterService : MeterReadingService.MeterReadingServiceBase
+    {
+        private ILogger<MeterService> _logger;
+
+        public MeterService(ILogger<MeterService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            var result = new StatusMessage()
+            {
+                Success = ReadingStatus.Failure
+            };
+
+            if(request.Successful == ReadingStatus.Success)
+            {
+                try
+                {
+                    foreach (var reading in request.Readings)
+                    {
+                        // Save to database
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    result.Message = $"Exception: {ex.Message}";
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
